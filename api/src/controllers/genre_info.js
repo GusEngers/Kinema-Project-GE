284c567dda7d6075805057dbd0e55468d86097c3
GusEngers <gustavoengers6441@gmail.com>
2023-04-17 18:11:36 -0300

    comentarios

--- api/src/controllers/genre_info.js
+++ api/src/controllers/genre_info.js
@@ -3,10 +3,12 @@ const { GENRES_MOVIES, GENRES_TV } = require('./genres');
 /**
  * Convierte un array con sólo id's de géneros a un array con nombre y id de los respectivos géneros
  * @param genres Array con id's de géneros
+ * @param mode Tipo de genero que queremos convertir (movie o tv)
  * @returns Array de objetos con nombre y id de los géneros
  */
 function genreInfo(genres, mode) {
-  if (mode === 'movie') return genres.map((genre) => GENRES_MOVIES[genre]);
+  if (mode.toLowerCase() === 'movie')
+    return genres.map((genre) => GENRES_MOVIES[genre]);
   return genres.map((genre) => GENRES_TV[genre]);
 }
 
