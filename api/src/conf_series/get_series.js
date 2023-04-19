e4c086fa75036f472b26a11260bd4e990f1c07bc
GusEngers <gustavoengers6441@gmail.com>
2023-04-19 12:26:13 -0300

    Comentario

--- api/src/conf_series/get_series.js
+++ api/src/conf_series/get_series.js
@@ -4,6 +4,11 @@ require('dotenv').config();
 
 const { API_KEY } = process.env;
 
+/**
+ * Obtiene una lista de 20 series populares de la API y luego verifica si están completas
+ * @param page Número de página
+ * @returns Array con las series que pasen la validación
+ */
 async function getSeries(page) {
   const API = `https://api.themoviedb.org/3/tv/popular?api_key=${API_KEY}&page=${page}`;
   console.log('Getting series from page', page);
